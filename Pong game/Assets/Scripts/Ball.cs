a47e42315502fa23a6243a16e7fe4456a6292127
swily123 <92625491+swily123@users.noreply.github.com>
2025-12-09 02:13:14 +0500

    added ball script

---
+++ Pong game/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    private Transform _transform;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        _rigidbody.velocity = velocity;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return _transform.position;
+    }
+}
